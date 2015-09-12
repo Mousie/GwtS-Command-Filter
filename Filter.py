36666dbfc0bc11ddcd2118ccd393b540e74d81d9
Chris <cdkihano@gmail.com>
2015-09-12 04:10:21 -0700

    Initial Commit

---
+++ Filter.py
@@ -0,0 +1,151 @@
+"""
+Filter for GwtS recordings. Checks to make sure all commands are correct and
+splits commands into different files.
+    - List with timestamps of only 9X commands.
+    - List with timestamps of only 55 commands.
+    - List with timestamps of both 9X and 55 commands.
+    - List with no timestamps of only unique 9X commands, ommiting delays and some 0C codes.
+    - List with no timestamps of only unique 55 commands.
+    - List of commands that didn't pass 55 or 9X CRC, likely from transmission errors or collisions.
+
+Version: 2015-09-07
+
+Use:
+    python Filter.py input_file_name #of_timestamp_pieces
+
+Example:
+    python Filter.py 2015-08-30.txt 2
+
+    Where 2015-08-30.txt is the name of the file to be processed and 2 is the number
+    of pieces where time codes occur (in this case it's 2, 2015-08-30 15:34:20.250)
+    before the actual commands occur.
+
+MousieMagic@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import GwtSUtils
+import sys
+
+
+def split_input(file, time_code_len):
+    """
+    Splits file input into four lists, the 9x commands, the 55 commands, and a combined 9x
+        55 commands that also includes timestamps, and a list of error commands.
+    :param file String Filename
+    :param time_code_len Number of timestamp blocks before the actual code.
+    :return: [][]String Four lists of strings that contain the combined 9x and 55 commands,
+                        the 9X commands, and the 55 commands, and a list of error commands.
+    """
+    command95 = list()
+    command9 = list()
+    command5 = list()
+    recording_errors = list()
+    with open(file, 'r') as file_input:
+        for file_line in file_input:
+            file_line = file_line.rstrip().split()
+            if not check_command(file_line[time_code_len:]):
+                recording_errors.append(file_line)
+                continue
+            if file_line[time_code_len][0] == '9':
+                command9.append(file_line)
+            if file_line[time_code_len][0] == '5':
+                command5.append(file_line)
+            command95.append(file_line)
+    return command95, command9, command5, recording_errors
+
+
+def check_command(command):
+    """ Checks 9X and 55 commands to make sure they're correct.
+    :param command []String 9X or 55 command
+    :return Boolean True if it's a correct command and False if not.
+    """
+    if command[0][0] == '9':
+        calculated_command = GwtSUtils.int_array_to_hex_str(GwtSUtils.encode9x(' '.join(command[1:-1])))
+    elif command[0][0] == '5':
+        calculated_command = GwtSUtils.int_array_to_hex_str(GwtSUtils.encode55(' '.join(command[2:-1])))
+    else:
+        return False
+    if calculated_command == ' '.join(command):
+        return True
+    return False
+
+
+def remove_repeats55(commands, time_stamp_len):
+    """ Removes repeat of 55 commands in a list.
+    :param commands []String List of 55 commands
+    :return []String List of 55 commands with repeats removed.
+    """
+    return set(command for command in [' '.join(command[time_stamp_len:]) for command in commands])
+
+
+def remove_repeats9x(commands, time_stamp_len):
+    """ Removes repeat of 9X commands in a list. Also removes repeats of commands with different delays.
+    :param commands []String List of 9X commands
+    :return []String List of 9X commands with repeats removed.
+    """
+    has_delays = set()
+    return_set = set()
+    for line_9x in [command for command in [' '.join(command[time_stamp_len:]) for command in commands]]:
+        if line_9x[3] == 'F' or line_9x[3:5] == '20':
+            has_delays.add(line_9x[5:-3])
+        elif line_9x[3:5] == '0C':
+            has_delays.add(line_9x[9:-3])
+        else:
+            return_set.add(line_9x)
+    for line_9x in has_delays:
+        return_set.add(encode9x(line_9x))
+    return return_set
+
+
+def encode9x(command):
+    """ Encodes a 9X command with correct 9X length code and CRC.
+    :param command String 9X command without
+    :return String Encoded 9X command
+    """
+    return GwtSUtils.int_array_to_hex_str(GwtSUtils.encode9x(command))
+
+
+def save_list(command_list, type_of_command, file_name, file_name_tail_to_remove=4):
+    with open(file_name[:-file_name_tail_to_remove]+'_'+type_of_command+'.txt', 'w') as output:
+        for line in sorted(command_list):
+            if type(line) is str:
+                output.write(line + '\n')
+            else:
+                output.write(' '.join(line) + '\n')
+
+
+def main(file_name, time_code_len):
+    commands95, commands9, commands55, errors = split_input(file_name, time_code_len)
+    save_list(commands95, '9X_And_55', file_name)
+    save_list(commands9, '9X', file_name)
+    save_list(commands55, '55', file_name)
+    commands55 = remove_repeats55(commands55, time_code_len)
+    save_list(commands55, '55_No_Repeats', file_name)
+    commands9 = remove_repeats9x(commands9, time_code_len)
+    save_list(commands9, '9X_No_Repeats', file_name)
+    save_list(errors, 'Errors', file_name)
+    # Uncomment below to show lines that didn't pass any kind of CRC in order of how frequent they appeared.
+    # I typically keep it low (around 10) to pick up any possible commands we're not scanning for normally
+    # but are broadcasted multiple times but it's usually just command collisions and transmission errors.
+    '''
+    from collections import Counter
+    num_of_items_to_show = 10
+    for item in Counter([' '.join(item[1:]) for item in errors]).most_common(num_of_items_to_show):
+        print(item)
+    '''
+
+if __name__ == '__main__':
+    main(sys.argv[1], int(sys.argv[2]))
---
+++ GwtSUtils.py
@@ -0,0 +1,136 @@
+"""
+GwtS Utilities for calculating 9X commands, 55 commands, IR timings, and sequencing of commands.
+
+MousieMagic@gmail.com
+
+Version: 2015-09-06
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+# Delays seem to vary. Commonly only up to FB but some go up to FF. I think it
+# might have something to do with command length and avoiding collisions but
+# I'm keeping it to a safe FB for now. Switch to the larger set if you'd like.
+#delays = ("FF", "FE", "FD", "FC", "FB", "FA", "F9", "F8", "F7", "F6", "F5", "F4", "F3", "F2", "F1", "20")
+delays = ("FB", "FA", "F9", "F8", "F7", "F6", "F5", "F4", "F3", "F2", "F1", "20")
+
+def encode9x(values):
+    """ Encodes a command with the appropriate 9X length code and CRC
+    :param values: int[] Commands in decimal format OR str[] commands in hex format OR a single str of commands in hex
+    :return: int[] 9x encoded commands in decimal format
+    """
+    if type(values) is str:
+        values = values.split()  # Split user input into str[]
+    if type(values) is list:
+        try:
+            if type(values[0]) is str:
+                values = [int(value, 16) for value in values]  # Convert str[] with hex values to int[]
+        except:
+            pass
+    values.insert(0, len(values) + 143)
+    values.append(crc_9x(values))
+    return values
+
+
+def crc_9x(values):
+    """ Calculate CRC for 9X commands
+    :param values: int[] Commands, with accompanying 9x length code, written in decimal format.
+    :return: int[] CRC value for 9X commands
+    """
+    crc = 0
+    for value in values:
+        crc ^= value
+        for num in range(8):
+            crc = (crc >> 1 ^ 0x8C if crc & 1 else crc >> 1)
+    return crc
+
+
+def encode55(values):
+    ''' Encodes a command with 55 AA code and CRC.
+    :param values: int[] Commands in decimal format
+    :return: int[] 55 encoded commands in decimal format
+    '''
+    values = [int(value, 16) for value in values.split()]
+    values.append(crc55(values))
+    return [0x55, 0xAA] + values
+
+
+def crc55(values):
+    ''' Calculates CRC for 55 commands
+    :param values: int[] command without 55 AA
+    :return: int[] CRC value for 55 commands
+    '''
+    return sum(values) % 256
+
+
+def encode_ir(values, message_width=417):
+    """ Encodes a complete command into IR compatible on/off time lengths.
+    :param values: int[] Command to be encoded for IR
+    :param message_width: int Optional variable to change the message width.
+    :return: int[] Encoded array of values for IR transmission.
+    """
+    return_list = []
+    for value in values:
+        # Commands always have initial on start bit
+        high = False
+        return_list.append(message_width)
+        # Cycle through bits for on/off bits and add/flip as needed.
+        for count in range(8):
+            if value >> count & 1 == high:
+                return_list[-1] += message_width
+            else:
+                return_list.append(message_width)
+                high = not high
+        # End with off bit
+        if high:
+            return_list[-1] += message_width
+        else:
+            return_list.append(message_width)
+    return return_list
+
+
+def int_array_to_hex_str(values):
+    """ Converts int[] to hex string
+    :param values: int[] Values to convert to hex string
+    :return: String of converted values
+    """
+    return ' '.join([hex(value).upper()[2:].zfill(2) for value in values])
+
+
+def ir_fancy_format(values):
+    """ Formats IR on/off lengths nicely
+    :param values: int[] of on/off IR lengths
+    :return: String of nicely formatted IR lengths.
+    """
+    return '{'+', '.join(str(value) for value in values)+'}'
+
+
+def generate_delays(command):
+    """ Generates timing and calculates delays of a command for a show.
+    :param command: str[] of time offset and commands
+    :return: dict() of using their timings as keys and the 9x encoded commands as content.
+    """
+    delay_dict = dict()
+    for index, delay in enumerate(delays):
+        # Calculate time advances
+        time = (int(command[0])-(len(delays)-index)*100)
+        if time < 0:  # If the time advance goes before 0 (start of show), omit it.
+                continue
+        # Put together delay code and command
+        command_list = [delay]
+        command_list.extend(command[1:])
+        # Add to dict the timing and encode the command for 9x with appropriate delays.
+        delay_dict[hex(time)[2:].upper().zfill(8)] = [hex(value)[2:].upper().zfill(2) for value in encode9x(command_list)]
+    return delay_dict
